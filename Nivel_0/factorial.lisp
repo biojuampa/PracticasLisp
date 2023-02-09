af4cd06ad05a6d2631f4c603c3ce1f39c3887ad5
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-02-09 15:36:37 -0300

    Factorial - Nivel 0

---
+++ Nivel_0/factorial.lisp
@@ -0,0 +1,13 @@
+; Clásica práctica de recursividad
+; con la "función factorial".
+
+(defun factorial (n)
+    (if (eq n 0)
+        1
+        (* n (factorial (- n 1)))
+    )
+)
+
+(set 'result (factorial 5))
+
+(print result)
